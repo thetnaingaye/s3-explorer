2d378bc3bc213450f0cb501d748138cb20ddbae2
Thet Naing Aye <mmu1tn@gmail.com>
2023-05-13 14:58:33 +0800

    style: fix eslint double quote

--- src/main/main.ts
+++ src/main/main.ts
@@ -8,25 +8,25 @@
  * When running `npm run build` or `npm run build:main`, this file is compiled to
  * `./src/main.js` using webpack. This gives us some performance wins.
  */
-import path from 'path';
-import { app, BrowserWindow, shell, ipcMain } from 'electron';
-import { autoUpdater } from 'electron-updater';
+import path from "path";
+import { app, BrowserWindow, shell, ipcMain } from "electron";
+import { autoUpdater } from "electron-updater";
 // import ChildProcess from 'child_process';
-import log from 'electron-log';
-import AWS from 'aws-sdk';
-import MenuBuilder from './menu';
-import { resolveHtmlPath } from './util';
+import log from "electron-log";
+import AWS from "aws-sdk";
+import MenuBuilder from "./menu";
+import { resolveHtmlPath } from "./util";
 
 AWS.config.getCredentials((err) => {
   if (err) console.log(err.stack);
   // credentials not loaded
   else {
-    console.log('Access key:', AWS.config?.credentials?.accessKeyId);
+    console.log("Access key:", AWS.config?.credentials?.accessKeyId);
   }
 });
 class AppUpdater {
   constructor() {
-    log.transports.file.level = 'info';
+    log.transports.file.level = "info";
     autoUpdater.logger = log;
     autoUpdater.checkForUpdatesAndNotify();
   }
@@ -34,7 +34,7 @@ class AppUpdater {
 
 let mainWindow: BrowserWindow | null = null;
 
-ipcMain.on('ipc-example', async (event, arg) => {
+ipcMain.on("ipc-example", async (event, arg) => {
   const msgTemplate = (pingPong: string) => `IPC test: ${pingPong}`;
   console.log(msgTemplate(arg));
 
@@ -44,10 +44,10 @@ ipcMain.on('ipc-example', async (event, arg) => {
   });
   s3.listBuckets((err, data) => {
     if (err) {
-      console.log('Error', err);
+      console.log("Error", err);
     } else {
-      console.log('Success', data.Buckets);
-      event.reply('ipc-example', data.Buckets);
+      console.log("Success", data.Buckets);
+      event.reply("ipc-example", data.Buckets);
     }
   });
   // const cmd = ChildProcess.spawnSync('aws', ['s3', 'ls'], {
@@ -58,22 +58,22 @@ ipcMain.on('ipc-example', async (event, arg) => {
   // event.reply('ipc-example', msgTemplate('pong'));
 });
 
-if (process.env.NODE_ENV === 'production') {
-  const sourceMapSupport = require('source-map-support');
+if (process.env.NODE_ENV === "production") {
+  const sourceMapSupport = require("source-map-support");
   sourceMapSupport.install();
 }
 
 const isDebug =
-  process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true';
+  process.env.NODE_ENV === "development" || process.env.DEBUG_PROD === "true";
 
 if (isDebug) {
-  require('electron-debug')();
+  require("electron-debug")();
 }
 
 const installExtensions = async () => {
-  const installer = require('electron-devtools-installer');
+  const installer = require("electron-devtools-installer");
   const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
-  const extensions = ['REACT_DEVELOPER_TOOLS'];
+  const extensions = ["REACT_DEVELOPER_TOOLS"];
 
   return installer
     .default(
@@ -89,8 +89,8 @@ const createWindow = async () => {
   }
 
   const RESOURCES_PATH = app.isPackaged
-    ? path.join(process.resourcesPath, 'assets')
-    : path.join(__dirname, '../../assets');
+    ? path.join(process.resourcesPath, "assets")
+    : path.join(__dirname, "../../assets");
 
   const getAssetPath = (...paths: string[]): string => {
     return path.join(RESOURCES_PATH, ...paths);
@@ -100,17 +100,17 @@ const createWindow = async () => {
     show: false,
     width: 1024,
     height: 728,
-    icon: getAssetPath('icon.png'),
+    icon: getAssetPath("icon.png"),
     webPreferences: {
       preload: app.isPackaged
-        ? path.join(__dirname, 'preload.js')
-        : path.join(__dirname, '../../.erb/dll/preload.js'),
+        ? path.join(__dirname, "preload.js")
+        : path.join(__dirname, "../../.erb/dll/preload.js"),
     },
   });
 
-  mainWindow.loadURL(resolveHtmlPath('index.html'));
+  mainWindow.loadURL(resolveHtmlPath("index.html"));
 
-  mainWindow.on('ready-to-show', () => {
+  mainWindow.on("ready-to-show", () => {
     if (!mainWindow) {
       throw new Error('"mainWindow" is not defined');
     }
@@ -121,7 +121,7 @@ const createWindow = async () => {
     }
   });
 
-  mainWindow.on('closed', () => {
+  mainWindow.on("closed", () => {
     mainWindow = null;
   });
 
@@ -131,7 +131,7 @@ const createWindow = async () => {
   // Open urls in the user's browser
   mainWindow.webContents.setWindowOpenHandler((edata) => {
     shell.openExternal(edata.url);
-    return { action: 'deny' };
+    return { action: "deny" };
   });
 
   // Remove this if your app does not use auto updates
@@ -143,10 +143,10 @@ const createWindow = async () => {
  * Add event listeners...
  */
 
-app.on('window-all-closed', () => {
+app.on("window-all-closed", () => {
   // Respect the OSX convention of having the application in memory even
   // after all windows have been closed
-  if (process.platform !== 'darwin') {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
@@ -155,7 +155,7 @@ app
   .whenReady()
   .then(() => {
     createWindow();
-    app.on('activate', () => {
+    app.on("activate", () => {
       // On macOS it's common to re-create a window in the app when the
       // dock icon is clicked and there are no other windows open.
       if (mainWindow === null) createWindow();
--- src/renderer/App.jsx
+++ src/renderer/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react';
-import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState } from "react";
+import { MemoryRouter as Router, Routes, Route } from "react-router-dom";
 // import icon from '../../assets/icon.svg';
-import './App.css';
+import "./App.css";
 
 function Hello() {
   const [buckets, setBuckets] = useState([]);
   const [loading, setLoading] = useState(false);
-  window.electron.ipcRenderer.once('ipc-example', (data) => {
+  window.electron.ipcRenderer.once("ipc-example", (data) => {
     // eslint-disable-next-line no-console
     // setBuckets(arg?.split('\n'));
     setBuckets(data);
@@ -14,22 +14,22 @@ function Hello() {
   });
   const refreshBuckets = () => {
     setLoading(true);
-    window.electron.ipcRenderer.sendMessage('ipc-example', 'refersh-buckets');
+    window.electron.ipcRenderer.sendMessage("ipc-example", "refersh-buckets");
   };
 
   return (
     <div style={{ padding: 10 }}>
       <h1>S3 Explorer (using aws-sdk and aws-cli)</h1>
       <h2>
-        Buckets{' '}
+        Buckets{" "}
         <button type="button" onClick={refreshBuckets}>
           Refresh
         </button>
-      </h2>{' '}
+      </h2>{" "}
       {loading ? (
         <div>Loading...</div>
       ) : (
-        <div style={{ maxHeight: '70vh', overflowY: 'scroll', width: '100%' }}>
+        <div style={{ maxHeight: "70vh", overflowY: "scroll", width: "100%" }}>
           <table>
             <tbody>
               {buckets.map((item, idx) => (
