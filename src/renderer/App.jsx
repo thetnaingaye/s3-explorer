a8ab4b9af20d3990e12b83557f2d4976a55fad08
Thet Naing Aye <mmu1tn@gmail.com>
2023-05-13 20:24:45 +0800

    style: update app icon

--- src/renderer/App.jsx
+++ src/renderer/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { MemoryRouter as Router, Routes, Route, Link } from "react-router-dom";
 // import icon from '../../assets/icon.svg';
 import "./App.css";
-import { Button, Table } from "antd";
+import { Button, Card, Table } from "antd";
 import TableObjects from "./TableObjects";
 
 function Hello() {
@@ -42,38 +42,62 @@ function Hello() {
       ),
     },
     {
-      title: "CreationDate",
+      title: "Creation Date",
       dataIndex: "CreationDate",
-      render: (text) => <span>{text.toString()}</span>,
+      render: (text) => (
+        <span style={{ color: "grey" }}>{text.toString()}</span>
+      ),
     },
   ];
   return (
-    <div style={{ padding: 10 }}>
-      <h1>S3 Explorer (using aws-sdk and aws-cli)</h1>
-      <h2>
-        Buckets{" "}
-        <Button type="primary" onClick={refreshBuckets}>
+    <Card
+      title={
+        <span>
+          Buckets <span>{buckets.length && `(${buckets.length})`}</span>
+        </span>
+      }
+      extra={[
+        <Button onClick={refreshBuckets} key="refresh">
           Refresh
-        </Button>
-      </h2>{" "}
+        </Button>,
+      ]}
+    >
       <Table
         rowKey="Name"
         columns={columns}
         dataSource={buckets}
         loading={loading}
-        size="middle"
+        size="small"
       />
-    </div>
+    </Card>
   );
 }
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route excat path="/" element={<Hello />} />
-        <Route path="/objects/:bucket" element={<TableObjects />} />
-      </Routes>
-    </Router>
+    <div>
+      <div
+        style={{
+          padding: 20,
+          borderBottom: "5px solid orange",
+          color: "#333",
+          fontSize: "125%",
+          fontWeight: 300,
+          letterSpacing: 1.1,
+          backgroundColor: "#f0f0f0",
+        }}
+      >
+        <strong>AWS S3 Explorer </strong>
+        <span style={{ fontSize: "50%", letterSpacing: 1 }}>
+          with aws-sdk and aws-cli
+        </span>
+      </div>
+      <Router>
+        <Routes>
+          <Route excat path="/" element={<Hello />} />
+          <Route path="/objects/:bucket" element={<TableObjects />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
--- src/renderer/TableObjects.jsx
+++ src/renderer/TableObjects.jsx
@@ -1,6 +1,12 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { Button, Table } from "antd";
+import { Button, Card, Table } from "antd";
+import {
+  DownloadOutlined,
+  FileOutlined,
+  FolderFilled,
+  HomeFilled,
+} from "@ant-design/icons";
 
 function TableObjects() {
   const { bucket } = useParams();
@@ -10,7 +16,8 @@ function TableObjects() {
   window.electron.ipcRenderer.once("ipc-s3", (data) => {
     // eslint-disable-next-line no-console
     console.log("objects", data);
-    setObjects([...data.contents, ...data.prefixes]);
+    const mergeData = [...data.contents, ...data.prefixes];
+    setObjects(mergeData.filter((x) => x));
     setLoading(false);
   });
   const handleListObjectsByBucket = (BucketName) => {
@@ -36,28 +43,54 @@ function TableObjects() {
     {
       title: "Name",
       dataIndex: "Key",
-      render: (text, row) => <Button type="link">{text || row?.Prefix}</Button>,
+      render: (text, row) => {
+        if (row?.Prefix) {
+          return (
+            <div>
+              <FolderFilled style={{ fontSize: "125%" }} />
+              <Button type="link">{row?.Prefix}</Button>
+            </div>
+          );
+        }
+        return (
+          <div>
+            <FileOutlined />
+            <span style={{ marginLeft: 20 }}>{text}</span>
+          </div>
+        );
+      },
     },
     {
       title: "Action",
       render: (text, row) => {
         if (!row?.Key) return null;
-        return <Button onClick={() => getObject(row.Key)}>download</Button>;
+        return (
+          <Button onClick={() => getObject(row.Key)} size="small">
+            <DownloadOutlined />
+            download
+          </Button>
+        );
       },
     },
   ];
   return (
-    <div style={{ padding: 10 }}>
-      <Button onClick={() => navigate("/")}>Home</Button>
-      <h1>Bucket: {bucket}</h1>
+    <Card
+      title={bucket}
+      extra={[
+        <Button key="home" onClick={() => navigate("/")}>
+          <HomeFilled />
+          Home
+        </Button>,
+      ]}
+    >
       <Table
         rowKey="Name"
         columns={columns}
         dataSource={objects}
         loading={loading}
-        size="middle"
+        size="small"
       />
-    </div>
+    </Card>
   );
 }
 
