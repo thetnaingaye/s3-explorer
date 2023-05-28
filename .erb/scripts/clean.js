ff23e3c7e9fe660d7a93f01d203b2031704534a5
Thet Naing Aye <mmu1tn@gmail.com>
2023-05-28 12:19:49 +0800

    fix: main.js and update quotes

--- .erb/scripts/clean.js
+++ .erb/scripts/clean.js
@@ -1,6 +1,6 @@
-import rimraf from 'rimraf';
-import fs from 'fs';
-import webpackPaths from '../configs/webpack.paths';
+import rimraf from "rimraf";
+import fs from "fs";
+import webpackPaths from "../configs/webpack.paths";
 
 const foldersToRemove = [
   webpackPaths.distPath,
